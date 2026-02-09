7ea0e39238a004f737f0b3bdaaa94cfe9f23c387
Senne Pieters <senne.pieters02@gmail.com>
2026-02-09 21:37:40 +0100

    wip

--- Linux/install-apps.sh
+++ Linux/install-apps.sh
@@ -1,9 +1,9 @@
-# update system
+# rank the mirrors & update system
+sudo cachyos-rate-mirrors
+paru -Syyu
 
-# rank the mirrors in cachyhelo
-# enable cachy update
-
-paru
+# enable update checker
+systemctl --user enable --now arch-update.timer
 
 # gaming files
 paru -S cachyos-gaming-meta
@@ -14,4 +14,4 @@ paru -S steam
 paru -S google-chrome
 paru -S visual-studio-code-bin
 paru -S obs-studio
-paru -S parsec-bins
+paru -S parsec-bin
